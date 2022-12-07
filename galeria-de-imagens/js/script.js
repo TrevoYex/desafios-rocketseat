2856a05e0b7d97c6ef63cf6faceb245baa9efad9
TrevoYex <estevam.neto07@gmail.com>
2022-12-07 19:11:32 -0300

    atualizações testes

--- galeria-de-imagens/js/script.js
+++ galeria-de-imagens/js/script.js
@@ -1,5 +1,7 @@
 import initThemeSwitch from './modules/theme-switch.js';
 import initScrollToSection from './modules/scroll-to-section.js';
+import initModalImages from './modules/full-screen-images.js';
 
 initThemeSwitch();
 initScrollToSection();
+initFullScreen();
