faa2d93279daf5a1b285b2f90ec5794efb3e0a17
TrevoYex <estevam.neto07@gmail.com>
2022-12-09 19:54:32 -0300

    full screen funcionando

--- galeria-de-imagens/js/modules/full-screen-images.js
+++ galeria-de-imagens/js/modules/full-screen-images.js
@@ -1,9 +1,27 @@
 export default function initFullScreen() {
     const imgs = document.querySelectorAll('img');
+    const close = document.querySelector('.close');
+    const view = document.querySelectorAll('.view');
+    const fullScreenImg = document.querySelector('#full-screen-img');
     
+    let i;
+
     imgs.forEach(img => {
         img.addEventListener('click', () => {
             document.querySelector('.img-container').classList.add('active');
-        })
-    })
+            // document.body.classList.add('full-screen-img');
+            fullScreenImg.src = img.src;
+            i = img.getAttribute('data-number');
+        });
+    });
+
+    view.forEach(item => {
+        item.addEventListener('click', () => {
+            document.querySelector('.img-container').classList.add('active');
+        });
+    });
+
+    close.addEventListener('click', () => {
+        document.querySelector('.img-container').classList.remove('active');
+    });
 }
